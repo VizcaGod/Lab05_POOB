3ea0d2694826b79a76c4a6ae6215787ace4576ee
Parralol <110953563+Parralol@users.noreply.github.com>
2023-04-21 12:34:03 -0500

    jugarod y tablero

--- lab05/Conecta4/src/domain/Jugador.java
+++ lab05/Conecta4/src/domain/Jugador.java
@@ -4,13 +4,57 @@ import java.util.ArrayList;
 import java.awt.Color;
 
 public class Jugador {
-    private int score;
     private ArrayList<Ficha> fichas;
     private String name;
     private Color color;
+    private int turno;
 
-    public Jugador() {
-
+    public Jugador(String name, Color color, int turno) {
+        this.name = name;
+        this.color = color;
+        this.turno = turno;
     }
 
+    public void play(int x, int y) {
+        Ficha ficha = new Ficha(x, y);
+        fichas.add(ficha);
+    }
+
+    public boolean gano(Ficha ficha) {
+        boolean res = false;
+        int cont = 0;
+        int[] guarda = ficha.getPos();
+
+        return res;
+    }
+
+    public ArrayList<Ficha> getFichas() {
+        return fichas;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getTurno() {
+        return turno;
+    }
+
+    public int getScore() {
+        return fichas.size();
+    }
+
+    // METODOS PRIVADOS
+
+    private boolean isThereNext(int[] guarda) {
+        boolean res = false;
+        for (Ficha a : fichas) {
+
+        }
+        return res;
+    }
 }
