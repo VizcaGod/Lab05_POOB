c6243e6319dcab8ff45f18dedf977a6477a304b8
Parralol <110953563+Parralol@users.noreply.github.com>
2023-04-28 23:00:12 -0500

    dominio finalizado

--- lab05/Conecta4/src/domain/Conecta4.java
+++ lab05/Conecta4/src/domain/Conecta4.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 /**
  * The test class Conecta4.
@@ -18,14 +19,57 @@ public class Conecta4 {
         tablero = new Tablero();
     }
 
+    /**
+     * agrega a un jugador a conecta4
+     * 
+     * @param name
+     * @param color
+     * @param turno
+     * @throws Conecta4Exception
+     */
     public void addJugador(String name, Color color, int turno) throws Conecta4Exception {
         tablero.addJugador(name, color, turno);
     }
 
+    /**
+     * realiza una jugada
+     * 
+     * @param x
+     * @param y
+     * @throws Conecta4Exception
+     */
     public void play(int x, int y) throws Conecta4Exception {
         boolean ganador = tablero.play(x, y);
-        if (ganador && tablero.getTurno() == 0) {
-            throw new Conecta4Exception(Conecta4Exception.GANADOR);
+        if (ganador) {
+            throw new Conecta4Exception(Conecta4Exception.GANADOR + tablero.getInTurnPlayerName());
         }
     }
+
+    /**
+     * retorna las fichas del tablero
+     * 
+     * @return
+     */
+    public ArrayList<Ficha> getFichas() {
+        return tablero.getFichas();
+    }
+
+    /**
+     * retorna el nombre del jugador en turno
+     * 
+     * @return
+     */
+    public String getNamePlayerInTurn() {
+        return tablero.getInTurnPlayerName();
+    }
+
+    /**
+     * retorna el turno
+     * 
+     * @return
+     */
+    public int getTurn() {
+        return tablero.getTurno();
+    }
+
 }
